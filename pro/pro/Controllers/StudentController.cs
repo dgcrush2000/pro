dbc10e5734f9ec40cda42927cc1d9cd11086db90
twelve <dgcrush2000@gmail.com>
2017-05-14 23:19:48 +0530

    First day work

---
+++ pro/pro/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using pro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pro.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Students> lst = new List<Students>();
+            lst.Add(new Students { Id = 1, Name = "aaaa", Address = "Mumbai" });
+            lst.Add(new Students { Id = 2, Name = "bbbb", Address = "Mumbai" });
+            lst.Add(new Students { Id = 3, Name = "cccc", Address = "Mumbai" });
+            return View(lst);
+        }
+    }
+}
---
+++ pro/pro/Models/Students.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pro.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public string Address { get; set; }
+    }
+}
